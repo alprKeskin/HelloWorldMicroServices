6414b7d524bbc41d4ad4d8c467768451ed1e7475
alprKeskin <99187103+alprKeskin@users.noreply.github.com>
2022-07-28 17:39:12 +0300

    ms_consumer init

---
+++ ms_consumer/src/main/java/io/github/alprkeskin/consumer/MsConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsConsumerApplication.class, args);
+    }
+
+}
---
+++ ms_consumer/src/test/java/io/github/alprkeskin/consumer/MsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alprkeskin.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
